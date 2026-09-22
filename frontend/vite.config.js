30e6a6a186b9ffca9fa87e274acf9c9b56ccf7c7
rudraksh229 <rudrakshpant2004@gmail.com>
2026-09-22 17:31:21 +0530

    Add PWA support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'GeoInsight AI',
+        short_name: 'GeoInsight AI',
+        description: 'AI-based land mapping and analysis system',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+      },
+    }),
   ],
 })
